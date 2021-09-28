eef7439983e49c3686c800e77df0f7d57d66f2f6
davidebortoloni <davidebortoloni@gmail.com>
2021-09-28 14:47:49 +0200

    GROUP BY - Query 2

--- queries.sql
+++ queries.sql
@@ -48,6 +48,9 @@ FROM `students`
 GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio:
+SELECT `office_address`, COUNT(*) AS `number_of_offices`
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame:
 
