29ef50bf2da7826d4402681dad81098aede6b9f8
davidebortoloni <davidebortoloni@gmail.com>
2021-09-28 14:51:01 +0200

    GROUP BY - Query 3

--- queries.sql
+++ queries.sql
@@ -53,5 +53,8 @@ FROM `teachers`
 GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame:
+SELECT `exam_id`, ROUND(AVG(`vote`), 2) AS `average`
+FROM `exam_student`
+GROUP BY `exam_id`
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento:
