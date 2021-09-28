75bfbe0792a57314e1b526cf0cb50826b8fdb087
davidebortoloni <davidebortoloni@gmail.com>
2021-09-28 14:43:22 +0200

    GROUP BY - Query 1

--- queries.sql
+++ queries.sql
@@ -43,6 +43,9 @@ WHERE `phone` IS NULL;
 -- QUERIES CON GROUP BY
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno:
+SELECT YEAR(`enrolment_date`) AS `year`, COUNT(*) AS `number_of_registered`
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio:
 
