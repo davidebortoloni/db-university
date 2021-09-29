d4794328bfec4b3ddfc4cbf9e8b76be41d321e3a
davidebortoloni <davidebortoloni@gmail.com>
2021-09-29 14:40:43 +0200

    JOIN - Query 6

--- queries_2.sql
+++ queries_2.sql
@@ -43,4 +43,17 @@ JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT `teachers`.*
+FROM `teachers`
+JOIN `course_teacher`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'
+GROUP BY `teachers`.`id`;
+
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
